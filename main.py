b0e1c6d724bfb3fb1db0bf0f66426c144b2d0c64
KevinFS1 <kevin@24-7mortgages.co.uk>
2023-05-14 17:53:27 +0100

    addedd more pages

--- main.py
+++ main.py
@@ -7,10 +7,15 @@ df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
     pdf.add_page()
+
+
     pdf.set_font(family="Times", size=20)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row['Topic'], align="L", ln=1)
     pdf.line(10, 20, 200, 20)
+
+    for i in range(row["Pages"]-1):
+        pdf.add_page()
 
 
 pdf.output("output.pdf")
